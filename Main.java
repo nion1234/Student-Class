4df369486e6dfc332a3e4c1addf64c8dcf0b5a27
Md.Rakib Rahman <56768955+nion1234@users.noreply.github.com>
2020-02-07 12:32:54 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Student s1=new Student();
+        Student s2=new Student();
+        Student s3=new Student();
+        s1.StudentName();
+        s2.StudentId();
+        s3.StudentDepartment();
+
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Student {
+    String name="Rakib";
+    String id="18-36684-1";
+    String department="CSE";
+    void StudentName(){
+        System.out.println("The Student name is"+" "+name);
+    }
+    void StudentId(){
+        System.out.println("The Student id is"+" "+id);
+    }
+    void StudentDepartment(){
+        System.out.println("The Student Department is"+" "+department);
+    }
+
+
+        }
